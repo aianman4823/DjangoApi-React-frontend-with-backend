4f099f3eb9bbff147fb1c44b83a800aafd230d22
aianman4823 <akitoaianman1@gmail.com>
2019-07-03 05:27:24 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// // Mockデータ
+// const list = [
+//   {
+//     id: 1,
+//     title: "1st todo",
+//     description: "Learn Django properly"
+//   },
+//   {
+//     id: 2,
+//     title: "Second item",
+//     description: "Learn Python."
+//   },
+//   {
+//     id: 3,
+//     title: "Learn HTTP",
+//     description: "It's important."
+//   }
+// ]
+
+class App extends Component{
+  state={
+    todos: []
+  };
+
+  componentDidMount(){
+    this.getTodos();
+  }
+
+  getTodos(){
+    axios.get('http://localhost:8000/api/')
+    .then(res=>{
+      this.setState({todos:res.data});
+    })
+    .catch(err=>{
+      console.log(err);
+    });
+  }
+
+  render(){
+    return(
+      <div>
+        {this.state.todos.map(item=>(
+          <div key={item.id}>
+            <h1>{item.title}</h1>
+            <p>{item.body}</p>
+          </div>
+        ))}
+      </div>
+    );
+  }
 }
-
 export default App;
